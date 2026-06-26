a9bfd2c9926c9dfb4a60b976cc2594d561f4c830
Sajal Kaundal <kaundal.sajal@rwit.io>
2026-06-26 18:25:00 +0530

    still figuring out the responsive design and how to plan the whole code so that it can be optimized

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import { Post } from '../data/home-page'
-import Image from 'next/image'
-import Link from 'next/link'
-import Typography from './typography/Typography'
+import React from "react";
+import { Post } from "../data/home-page";
+import Image from "next/image";
+import Link from "next/link";
+import Typography from "./typography/Typography";
 
-function BlogCard({ post }: {
-  post: Post
-}) {
+function BlogCard({ post }: { post: Post }) {
   return (
-    <div className='container max-w-100'>
+    <div className="container max-w-100">
       <div>
-        <Image alt='Post Image' src={post.image} width={400} height={360} />
+        <Image alt="Post Image" src={post.image} width={400} height={360} />
       </div>
-      <div className='mt-10 flex gap-3'>
-        <Typography variant='overline' className='font-bold'>
+      <div className="mt-10 flex gap-3">
+        <Typography variant="overline" className="font-bold">
           {post.category}
         </Typography>
-        <Typography variant='overline' color="text-muted" className='font-medium'>
+        <Typography
+          variant="overline"
+          color="text-muted"
+          className="font-medium"
+        >
           {post.date}
         </Typography>
       </div>
-      <div className='mt-4'>
-        <Typography variant='h6' className='font-bold'>
+      <div className="mt-4">
+        <Typography variant="h6" className="font-bold">
           {post.title}
         </Typography>
       </div>
-      <div className='mt-4'>
-        <Typography variant='body-sm' color='text-secondary'>
+      <div className="mt-4">
+        <Typography variant="body-sm" color="text-secondary">
           {post.description}
         </Typography>
       </div>
-      <div className='mt-4.75'>
+      <div className="mt-4.75">
         <Link href={`blog/${post.id}`}>
-          <Typography variant='body-lg' color='primary' className='underline font-heading font-bold'>
+          <Typography
+            variant="body-lg"
+            color="primary"
+            className="underline font-heading font-bold"
+          >
             Read More...
           </Typography>
         </Link>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
-export default BlogCard
+export default BlogCard;
--- src/app/components/FeaturedBlog.tsx
+++ src/app/components/FeaturedBlog.tsx
@@ -1,42 +1,46 @@
-import Image from 'next/image'
-import React from 'react'
-import { featuredBlog } from '../data/home-page'
-import Button from './ui/Button'
-import Typography from './typography/Typography'
+import Image from "next/image";
+import React from "react";
+import { featuredBlog } from "../data/home-page";
+import Button from "./ui/Button";
+import Typography from "./typography/Typography";
 function FeaturedBlog() {
   return (
-    <div className='contianer relative max-w-308 mt-32.75'>
-      <Image alt="Featured Blog Image" src={featuredBlog.image} height={576} width={1232} />
-      <div className='absolute bottom-0 right-0 max-w-230  rounded-2xl p-8 bg-white'>
-        <div className='flex gap-3'>
-          <Typography variant='overline' className='font-bold'>
+    <div className="container max-w-308 relative mt-32.75 pb-32">
+      <Image
+        alt="Featured Blog Image"
+        src={featuredBlog.image}
+        className="w-full h-auto"
+        height={576}
+        width={1232}
+      />
+
+      <div className="container md:absolute md:bottom-0 md:right-0 md:w-[70%] lg:w-[65%] h-[50%] rounded-2xl p-8 pr-26 bg-white">
+        <div className="flex gap-3">
+          <Typography variant="overline" className="font-bold">
             {featuredBlog.category}
           </Typography>
-          <Typography variant='overline' className='text-text-muted'>
+          <Typography variant="overline" className="text-text-muted">
             {featuredBlog.date}
           </Typography>
         </div>
-        <div className='mt-4'>
-          <Typography variant='h4' className='font-bold leading-11.25'>
+        <div className="mt-4">
+          <Typography variant="h4" className="font-bold leading-11.25">
             {featuredBlog.title}
           </Typography>
         </div>
-        <div className='mt-3.5'>
-          <Typography variant='body-sm'>
-            {featuredBlog.description}
-          </Typography>
+        <div className="mt-3.5">
+          <Typography variant="body-sm">{featuredBlog.description}</Typography>
         </div>
-        <div className='mt-12'>
-          <Button variant='purpleOutline' className='w-31.25'>
-            <Typography variant='button' color='primary'>
+        <div className="mt-12">
+          <Button variant="purpleOutline" className="h-10 w-31.25">
+            <Typography variant="button" color="primary">
               {featuredBlog.buttonText}
             </Typography>
           </Button>
         </div>
       </div>
     </div>
-
-  )
+  );
 }
 
-export default FeaturedBlog
+export default FeaturedBlog;
